326cb7229607e221a030d47de53c8248bb954fca
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2022-10-08 10:28:21 -0500

    login routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { requireAuth } = require('../../utils/auth.js');
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 // router.post('/test', function (req, res) {
 //     res.json({ requestBody: req.body });
 // });
