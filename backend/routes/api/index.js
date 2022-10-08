2c3fbafbfde829baca4aa571e3162e29c3a62f9b
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2022-10-08 09:26:53 -0500

    auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,39 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
 
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
+// // GET /api/set-token-cookie
 
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo'
+//         },
+//     })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 module.exports = router;
