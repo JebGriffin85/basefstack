b17b4c0a63cd7c23d5e4065cc586ab077f425265
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2022-10-12 13:43:21 -0500

    starting level routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const levelRouter = require('./level.js');
 
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/level', levelRouter)
 
 module.exports = router;
---
+++ backend/routes/api/level.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { handleValidationErrors } = require('../../utils/validation');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Level, User } = require('../../db/models');
+
+const router = express.Router();
+
+router.get('/getLevel/:id', asyncHandler(async (req, res) => {
+    const levelId  = parseInt(req.params.id)
+    // let levelId = 1;
+    if (levelId) {
+        const level = await Level.findOne({
+        where: { levelId },
+        include: User
+    });
+    if(level){
+        return res.json(level);
+
+    }
+}
+res.send('could not find')
+}));
+
+module.exports = router;
