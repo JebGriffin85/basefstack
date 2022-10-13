a75be8a82eda3ed5eb777e697e879c61bb80b5d1
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2022-10-13 13:12:40 -0500

    nav work

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -45,11 +45,32 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-export default function MenuAppBar() {
+
+
+export default function Navigation({ isLoaded }) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#fffff" // This is an orange looking color
+            },
+            secondary: {
+                main: "#ffcc80" //Another orange-ish color
+            }
+        }
+    });
+
+
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
-
+    const sessionUser = useSelector(state => state.session.user);
+    let user;
+       if (sessionUser) {
+        user = sessionUser.username
+    }
     const handleChange = (event) => {
         setAuth(event.target.checked);
     };
@@ -62,20 +83,10 @@ export default function MenuAppBar() {
         setAnchorEl(null);
     };
 
-    return (
+    return (isLoaded &&
         <Box sx={{ flexGrow: 1 }}>
-            <FormGroup>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={auth}
-                            onChange={handleChange}
-                            aria-label="login switch"
-                        />
-                    }
-                    label={auth ? 'Logout' : 'Login'}
-                />
-            </FormGroup>
+           
+                <ThemeProvider theme={theme}>
             <AppBar position="static">
                 <Toolbar>
                     
@@ -90,7 +101,7 @@ export default function MenuAppBar() {
                 
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Photos        <Menu
+                        Menu        <Menu
                             id="menu-appbar"
                             anchorEl={anchorEl}
                             anchorOrigin={{
@@ -110,39 +121,31 @@ export default function MenuAppBar() {
                         </Menu>
                     </Typography>
                     {auth && (
-                        <div>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <AccountCircle />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={handleClose}>My account</MenuItem>
-                            </Menu>
+                        
+                            <div> 
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: "inline", whiteSpace: "noWrap" }} noWrap={true}>
+                                Welcome {user}
+                            </Typography>
+                                <FormGroup>
+                                    <FormControlLabel
+                                        control={
+                                            <Switch
+                                                size="small"
+                                                checked={auth}
+                                                onChange={handleChange}
+                                                aria-label="login switch"
+                                                color="warning"
+                                            />
+                                        }
+                                        label={auth ? 'Logout' : 'Login'}
+                                    />
+                                </FormGroup>
                         </div>
+                        
                     )}
                 </Toolbar>
             </AppBar>
+                </ThemeProvider>
         </Box>
     );
 }
